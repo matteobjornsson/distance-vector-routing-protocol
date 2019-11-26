4ff0c55954b8a67a3ad3a6aeaa8e56736349d50f
matteobjornsson <matteobjornsson@gmail.com>
2019-11-25 22:10:59 -0700

    added message

--- simulation_3.py
+++ simulation_3.py
@@ -9,7 +9,7 @@ from typing import *
 
 ##configuration parameters
 router_queue_size = 0 #0 means unlimited
-simulation_time = 40   #give the network sufficient time to execute transfers
+simulation_time = 20   #give the network sufficient time to execute transfers
 
 if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads at the end
@@ -100,6 +100,8 @@ if __name__ == '__main__':
 
     #send packet from host 1 to host 2
     host_1.udt_send('H2', 'MESSAGE_FROM_H1')
+
+    host_2.udt_send('H1', 'MESSAGE FROM THE OTHERSIIIIIIIIIIIIIIIIDE')
     sleep(simulation_time)
     
     
