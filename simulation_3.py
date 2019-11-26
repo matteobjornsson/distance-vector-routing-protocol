eb58523a8f3a6714ba2b351167ea9e65a3d4acc5
Jerad Hoy <jeradhoy@gmail.com>
2019-11-25 22:31:19 -0700

    changed sim time in sim3

--- simulation_3.py
+++ simulation_3.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     #send packet from host 1 to host 2
     host_1.udt_send('H2', 'MESSAGE_FROM_H1')
     host_2.udt_send('H1', 'REPLY_FROM_H2')
-    sleep(simulation_time)
+    sleep(5)
     
     
     #join all threads
